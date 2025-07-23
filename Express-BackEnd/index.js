d291110d93d0415c84ed10169e1712bfb87a4406
zakie khan <121384625+zakie2003@users.noreply.github.com>
2025-07-23 14:28:02 +0530

    redployment

--- Express-BackEnd/index.js
+++ Express-BackEnd/index.js
@@ -14,7 +14,7 @@ const port=process.env.PORT ?? 3000;
 const app=express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://illustrious-dragon-77f03d.netlify.app"],
+  origin: ["*"],
   methods: ["GET", "POST"],
   credentials: true
 }));
@@ -23,7 +23,7 @@ app.use(cors({
 const server=http.createServer(app)
 const io=new Server(server,{
   cors: {
-    origin: ["http://localhost:5173", "https://illustrious-dragon-77f03d.netlify.app"],
+    origin: ["*"],
     methods: ["GET", "POST"]
   }
 });
